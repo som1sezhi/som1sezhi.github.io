f18c0b18d5e5aedd612053b2bfd677361faae06f
som1sezhi <s89jssy22@gmail.com>
2023-12-27 03:32:47 -0500

    initial commit, added pages for notitg files

---
+++ notitg-files/contents.js
@@ -0,0 +1,53 @@
+const contents = [
+  {
+    title: "feel",
+    artist: "kamome sano feat. mami",
+    date: "2022-01-29",
+    links: {
+      yt: "https://www.youtube.com/watch?v=jukiUGy8O_A",
+      dl: "https://www.dropbox.com/scl/fi/aeuluqhzol585ec3x80gh/feel.zip?rlkey=wqabhtvss7f7qyu6yem75ty4g&dl=0",
+    },
+  },
+  {
+    title: "BADSECRET",
+    artist: "BLACKTIGER",
+    date: "2022-05-10",
+    bannerURL: "media/BADSECRET.png",
+    links: {
+      yt: "https://www.youtube.com/watch?v=433qH5zGono",
+      dl: "https://www.dropbox.com/scl/fi/yugji6soitkv9ccspf5v8/BADSECRET.zip?rlkey=73b0zijakjrdcjvmr9qdjczwg&dl=0",
+    },
+  },
+  {
+    title: "YUMEUTSUTSU",
+    artist: "sky_delta feat. shully",
+    date: "2022-06-25",
+    bannerURL: "media/YUMEUTSUTSU.png",
+    pack: "Mod Jam Rex",
+    links: {
+      yt: "https://www.youtube.com/watch?v=5dVkMgm7RbI",
+      pack: "https://www.dropbox.com/scl/fo/5ti7swzzzt1extjf412fc/h?dl=0&rlkey=1y67h8z1nkl5o96ejyce8f96w",
+    },
+  },
+  {
+    title: "Clumsy thoughts",
+    artist: "猫叉Master feat. *spiLa*",
+    date: "2023-07-16",
+    bannerURL: "media/Clumsy thoughts.png",
+    pack: "Mod Jam Adoptables",
+    links: {
+      yt: "https://www.youtube.com/watch?v=9cDadpiu9Y4",
+      pack: "https://drive.google.com/file/d/1U4Y85Sj6vcIWBm6DGYMIeGGesZkoNSFP/view",
+    },
+  },
+  {
+    title: "Step This Way",
+    artist: "nanobii",
+    date: "2023-08-06",
+    bannerURL: "media/Step This Way.png",
+    links: {
+      yt: "https://www.youtube.com/watch?v=qh0pbLjdiPc",
+      dl: "https://www.dropbox.com/scl/fi/nt6sxfy32i8tg884ybfoh/Step_This_Way.zip?rlkey=7lkwuouyilgih3babr8yotyk4&dl=0",
+    },
+  },
+];
---
+++ notitg-files/populate.js
@@ -0,0 +1,96 @@
+(function () {
+  "use strict";
+
+  const LINK_TYPE_TO_ICON = {
+    yt: "media/YouTube.svg",
+    dl: "media/Download.svg",
+    pack: "media/Download.svg",
+  };
+
+  const LINK_TYPE_TO_TEXT = {
+    yt: "Watch on YouTube",
+    dl: "Download file",
+    pack: "Download pack (includes this file)",
+  };
+
+  function createBannerElem(bannerURL, ytLink) {
+    let elem;
+    if (ytLink) {
+      elem = document.createElement("a");
+      elem.href = ytLink;
+      elem.target = "_blank";
+      elem.rel = "noreferrer noopener";
+    } else {
+      elem = document.createElement("span");
+    }
+    if (bannerURL) {
+      elem.innerHTML = `
+        <img src="${bannerURL}" class="glow-on-hover" />
+      `;
+    } else {
+      elem.innerHTML = `
+        <div class="file-bn-empty glow-on-hover">[no banner]</div>
+      `;
+    }
+    return elem;
+  }
+
+  function createLinkButton(type, link) {
+    const iconURL = LINK_TYPE_TO_ICON[type];
+    const text = LINK_TYPE_TO_TEXT[type];
+    let elem = document.createElement("span");
+    elem.className = "link-btn glow-on-hover";
+    elem.innerHTML = `
+      <a href="${link}" target="_blank" rel="noreferrer noopener">
+        <img class="link-icon" src="${iconURL}" />
+        ${text}
+      </a>
+    `;
+    return elem;
+  }
+
+  function createFileListItemElem(fileInfo) {
+    let elem = document.createElement("div");
+    elem.className = "rounded-box file-list-item";
+    elem.innerHTML = `
+      <div class="file-bn-container"></div>
+      <div class="file-info-container">
+        <h2 class="file-title">
+          ${fileInfo.title}
+          <span class="file-title-artist-divider">/</span>
+          <span class="file-artist">${fileInfo.artist}</span>
+        </h2>
+        <p>
+          <span class="gray">released:</span> ${fileInfo.date}<br/>
+          ${
+            fileInfo.pack
+              ? `<span class="gray">pack:</span> ${fileInfo.pack}`
+              : `<span class="gray">single release</span>`
+          }
+        </p>
+        <p class="file-link-list"></p>
+      </div>
+    `;
+    const bnContainer = elem.querySelector(".file-bn-container");
+    bnContainer.appendChild(
+      createBannerElem(fileInfo.bannerURL, fileInfo.links.yt)
+    );
+    const linkList = elem.querySelector(".file-link-list");
+    ["yt", "dl", "pack"].forEach((type) => {
+      if (fileInfo.links[type]) {
+        const linkElem = createLinkButton(type, fileInfo.links[type]);
+        linkList.appendChild(linkElem);
+      }
+    });
+    return elem;
+  }
+
+  window.onload = function () {
+    const fileListElem = document.getElementById("file-list");
+    contents.reverse();
+    contents.forEach((fileInfo) => {
+      let elem = createFileListItemElem(fileInfo);
+      fileListElem.appendChild(elem);
+    });
+  };
+})();
